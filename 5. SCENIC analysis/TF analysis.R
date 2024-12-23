790d055efffa437898c383fb3aa91007e741dc74
Tao luo <131349324+sysu-Max@users.noreply.github.com>
2024-12-23 16:02:15 +0800

    Update TF analysis.R

--- 5. SCENIC analysis/TF analysis.R
+++ 5. SCENIC analysis/TF analysis.R
@@ -30,11 +30,11 @@ library(tidyverse)
 
 
 ####--scenic find clusters---
-donor_tsv <- openxlsx::read.xlsx('/data1/home/wanglh/CLT_IFLT/2.Analysis/merged/SNP/tsv/donor_tsv.xlsx')
-recipient_tsv <- openxlsx::read.xlsx('/data1/home/wanglh/CLT_IFLT/2.Analysis/merged/SNP/tsv/recipient_tsv.xlsx')
+donor_tsv <- openxlsx::read.xlsx('~/CLT_IFLT/2.Analysis/merged/SNP/tsv/donor_tsv.xlsx')
+recipient_tsv <- openxlsx::read.xlsx('~/CLT_IFLT/2.Analysis/merged/SNP/tsv/recipient_tsv.xlsx')
 
-load(file = '/data1/home/wanglh/CLT_IFLT/2.Analysis/merged/Annotation_data.RData')
-load(file = '/data1/home/wanglh/CLT_IFLT/2.Analysis/merged/Annotation_data.RData')
+load(file = '~/CLT_IFLT/2.Analysis/merged/Annotation_data.RData')
+load(file = '~/CLT_IFLT/2.Analysis/merged/Annotation_data.RData')
 subset_cells$plot_clusters <- subset_cells$new_clusters %>% as.character()
 subset_cells$identity <- subset_cells$sample.group %>% as.character()
 subset_cells$identity[row.names(subset_cells@meta.data) %in% donor_tsv$new_barcode] <- 'donor'
@@ -43,13 +43,13 @@ subset_cells <- subset(subset_cells, identity %in% c('donor','recipient')) ###åˆ
 
 subset_cells$identity_sample <- paste(subset_cells$identity,merged_CD4_7$sample.group, sep = '-')
 
-setwd('/data1/home/wanglh/CLT_IFLT/2.Analysis/merged/Scenic/SNP')
+setwd('~/CLT_IFLT/2.Analysis/merged/Scenic/SNP')
 subset_cells_selected <- subset(subset_cells,  plot_clusters == 'target_clusters') # selected target clusters
 Idents(subset_cells_selected) <- subset_cells_selected$identity_sample
 scenic_cells <- subset(subset_cells_selected, cells=WhichCells(subset_cells_selected, downsample=500))
 
 ####
-regulonAUC <- readRDS(file = '/data1/home/wanglh/CLT_IFLT/2.Analysis/merged/Scenic/SNP/regulonAUC.rds')
+regulonAUC <- readRDS(file = '~/CLT_IFLT/2.Analysis/merged/Scenic/SNP/regulonAUC.rds')
 scenic_cells@assays$RNA@data <- regulonAUC@assays@data@listData$AUC
 genes_modified <- scenic_cells %>% row.names %>% gsub(., pattern="_extended*", replacement = "")
 rownames(scenic_cells@assays$RNA@data) <- genes_modified
